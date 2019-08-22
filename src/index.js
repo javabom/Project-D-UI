d0866b40541dcf3dddef4ef6e535e3c64bcfb524
daengdaengLee <gunho1020@gmail.com>
2019-08-22 19:52:38 +0900

    material 디자인 설정

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Button from "@material-ui/core/Button";
 import "typeface-roboto";
 
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<div>hello world</div>, document.getElementById("root"));
+ReactDOM.render(
+  <Fragment>
+    <CssBaseline />
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  </Fragment>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
