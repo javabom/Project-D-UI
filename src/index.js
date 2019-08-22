0cb40fde089016d47d03f8b7a20a5b10a4e3151e
daengdaengLee <gunho1020@gmail.com>
2019-08-22 19:54:27 +0900

    react router 설정

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Button from "@material-ui/core/Button";
 import "typeface-roboto";
@@ -7,12 +8,14 @@ import "typeface-roboto";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <Fragment>
-    <CssBaseline />
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-  </Fragment>,
+  <Router>
+    <Fragment>
+      <CssBaseline />
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </Fragment>
+  </Router>,
   document.getElementById("root")
 );
 
