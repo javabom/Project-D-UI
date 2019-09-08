6b1ba852794db83d31b946e7a44b6e5aaa0d7e39
daengdaengLee <gunho1020@gamil.com>
2019-09-08 22:40:14 +0900

    withErrorBoundary HOC 생성

---
+++ src/hocs/with-error-boundary/with-error-boundary.default-params.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const FallbackCompDefault = ({ error, originalProps, onReset }) => (
+  <div>Something broken</div>
+);
+
+export const onErrorDefault = (error, info) => {};
