feb7b628e57b0e1273690edc88578a01bd452044
daengdaengLee <gunho1020@gmail.com>
2019-09-15 14:52:49 +0900

    App 컴포넌트에서 컨텍스트 주입

--- src/app.presenter.js
+++ src/app.presenter.js
@@ -3,17 +3,21 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import "typeface-roboto";
 
+import { ContextProvider } from "./contexts";
+
 import SigninPage from "./components/pages/signin-page/signin-page";
 
 const App = () => (
   <Router>
-    <Fragment>
-      <CssBaseline />
-      <Switch>
-        <Route path="/signin" component={SigninPage} />
-        <Route component={() => <div>default page</div>} />
-      </Switch>
-    </Fragment>
+    <ContextProvider>
+      <Fragment>
+        <CssBaseline />
+        <Switch>
+          <Route path="/signin" component={SigninPage} />
+          <Route component={() => <div>default page</div>} />
+        </Switch>
+      </Fragment>
+    </ContextProvider>
   </Router>
 );
 
